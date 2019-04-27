9037093af0ff63ec4513845ddd4dc96126d8c40c
Chan Jung <admin@Admins-MacBook-Pro.local>
2019-04-27 11:42:54 -0500

    init commit

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Chan Jung on 4/27/19.
+//  Copyright © 2019 Chan Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var answerArray : Array = ["yes", "no", "follow your heart", "don't follow your heart", "think twice"]
+    var answerInex = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
