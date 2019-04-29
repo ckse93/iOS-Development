b8df5bc70e59c4cb05a5eddb3b800a9ff7a4fc6f
ckse93 <ckse93@gmail.com>
2019-04-29 13:45:41 -0500

    implemented sound playing functionality

--- Xylophone-iOS12-master/Xylophone/ VIewController.swift
+++ Xylophone-iOS12-master/Xylophone/ VIewController.swift
@@ -7,18 +7,27 @@
 //
 
 import UIKit
+import AVFoundation  // foundation for audio visual components
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
+    var AudioPlayer : AVAudioPlayer!  // audio player variable
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-
     @IBAction func notePressed(_ sender: UIButton) {
-        print (sender.tag)  // track down which one was the signal from, returns the tag 
+        print (sender.tag)  // track down which one was the signal from, returns the tag
+        var note_number = "note"+"\(sender.tag)"
+        let soundURL = Bundle.main.url(forResource: note_number, withExtension: "wav")  //setup the location of the local wav file
+        
+        do {
+            AudioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        AudioPlayer.play()
     }
     
   
