5a34c31050c5b8185add87a102f1340e977f716f
ckse93 <ckse93@gmail.com>
2019-04-30 23:52:58 -0500

    Question class added as well as usage example.

--- Quizzler-iOS12-master/Quizzler/Question.swift
+++ Quizzler-iOS12-master/Quizzler/Question.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+class Question {
+    let questionText : String
+    let answer : Bool
+    init(text : String, correctAnswer : Bool) {  // initalizaion method
+        questionText = text
+        answer = correctAnswer
+    }
+}
+
+class myOtherClass {
+    let question = Question(text: "what is the meaning of life?" , correctAnswer: true)
+    // this is how you use it. 
+}
