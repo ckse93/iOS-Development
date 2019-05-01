5b49e0a06e3d5cf1eee4169840d844eb9abaa9b4
ckse93 <ckse93@gmail.com>
2019-05-01 00:26:13 -0500

    still need to implement checkAnswer functionality

--- Quizzler-iOS12-master/Quizzler/ViewController.swift
+++ Quizzler-iOS12-master/Quizzler/ViewController.swift
@@ -11,21 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestion = QuestionBank()
+    var pickedAnswer : Bool = false
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
-    override func viewDidLoad() {
+    override func viewDidLoad() {  // will run once when the app is loaded
         super.viewDidLoad()
-        
+        let firstQuestion = allQuestion.list[0]
+        questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if (sender.tag == 1) {
+            pickedAnswer = true
+        } else {
+            pickedAnswer = false
+        }
+        
+        
     }
     
     
