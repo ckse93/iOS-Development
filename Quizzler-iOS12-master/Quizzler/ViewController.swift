d5841b6be011a6d32207d41ebf33482353743ddd
ckse93 <ckse93@gmail.com>
2019-05-01 21:24:07 -0500

    ver2

--- Quizzler-iOS12-master/Quizzler/ViewController.swift
+++ Quizzler-iOS12-master/Quizzler/ViewController.swift
@@ -18,11 +18,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
+    var i = 0
+    var score = 0
     
     override func viewDidLoad() {  // will run once when the app is loaded
         super.viewDidLoad()
         let firstQuestion = allQuestion.list[0]
         questionLabel.text = firstQuestion.questionText
+        i += 1
     }
 
 
@@ -32,23 +35,40 @@ class ViewController: UIViewController {
         } else {
             pickedAnswer = false
         }
-        
-        
+        checkAnswer()
+        updateUI()
     }
     
     
     func updateUI() {
-      
+         if (i < allQuestion.list.count) {
+            questionLabel.text = allQuestion.list[i].questionText
+            progressLabel.text = "\(i)"
+            nextQuestion()
+         } else {
+            return
+        }
     }
     
 
     func nextQuestion() {
-        
+        i+=1
     }
     
     
     func checkAnswer() {
-        
+        if (i < allQuestion.list.count) {
+            let correctAnswer = allQuestion.list[i].answer
+            if (correctAnswer == pickedAnswer) {
+                score += 1
+                print ("correct")
+            } else {
+                print("wrong")
+            }
+            print (i)
+        } else {
+            startOver()
+        }
     }
     
     
