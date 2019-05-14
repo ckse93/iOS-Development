18450901b0af263ddfa3ea77b24564ab26cfb0d2
ckse93 <ckse93@gmail.com>
2019-05-14 17:40:53 -0500

    adding new one

---
+++ viewTesting/viewTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewTesting
+//
+//  Created by Chan Jung on 5/14/19.
+//  Copyright © 2019 Chan Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
