af4009e9b0984bcf1a062c80714db8d8d2ad969b
ckse93 <ckse93@gmail.com>
2019-05-16 09:49:52 -0500

    adding skeleton for classes and objhects

---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Chan Jung on 5/16/19.
+//  Copyright © 2019 Chan Jung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
