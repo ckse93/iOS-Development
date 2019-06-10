ed520e0ffb1db415d271313f314e85fe6abbfd9b
ckse93 <ckse93@gmail.com>
2019-06-10 17:45:36 -0500

    modulized bubblesort.h

---
+++ bubblesort.h
@@ -0,0 +1,29 @@
+//
+//  bubblesort.cpp
+//  
+//
+//  Created by Chan Jung on 6/10/19.
+//
+
+#include <stdio.h>
+
+
+void bubbleSort (std::vector<int>& vec, bool reverse) {
+    int len = vec.size();
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len-1; j++) {
+            if (vec[j] > vec[j+1]) {
+                int swap = vec[j];
+                vec[j] = vec[j+1];
+                vec[j+1] = swap;
+            }
+        }
+    }
+    if (reverse == true) {
+        for (int i = 0; i < (int)len/2; i++){
+            int swap = vec[i];
+            vec[i] = vec[len-i-1];
+            vec[len-i-1] = swap;
+        }
+    }
+}
--- sorting.cpp
+++ sorting.cpp
@@ -8,37 +8,10 @@
 #include <iostream>
 #include <vector>
 #include "sorting.hpp"
+#include "bubblesort.h"
 
 
-void displayVector (std::vector<int> arr) {
-    std::cout << "the array is : ";
-    int length = arr.size();
-    for (int i =0; i < length; i++) {
-        std::cout << arr[i] << ", ";
-    }
-    std::cout << "\n";
-}
-
-void bubbleSort (std::vector<int>& vec, bool reverse) {
-    int len = vec.size();
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len-1; j++) {
-            if (vec[j] > vec[j+1]) {
-                int swap = vec[j];
-                vec[j] = vec[j+1];
-                vec[j+1] = swap;
-            }
-        }
-    }
-    if (reverse == true) {
-        for (int i = 0; i < (int)len/2; i++){
-            int swap = vec[i];
-            vec[i] = vec[len-i-1];
-            vec[len-i-1] = swap;
-        }
-    }
-}
-
+void displayVector (std::vector<int> arr);
 
 int main (){
     std::vector<int> vec = {1,4,3,2,5,7,5,3,1,2,8,9,6,5,32,5,645,2,2,234,0,867,1};
@@ -50,4 +23,13 @@ int main (){
     displayVector(vec);
     
     return 0;
+}
+
+void displayVector (std::vector<int> arr) {
+    std::cout << "the array is : ";
+    int length = arr.size();
+    for (int i =0; i < length; i++) {
+        std::cout << arr[i] << ", ";
+    }
+    std::cout << "\n";
 }
