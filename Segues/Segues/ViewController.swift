4c558a0163b582fee49ec7e6ff2731d6b8e69250
ckse93 <ckse93@gmail.com>
2019-07-08 16:43:44 -0500

    made segue

--- Segues/Segues/ViewController.swift
+++ Segues/Segues/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
 
 }
 
