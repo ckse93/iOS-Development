bff91356d3c52455d31708044e7f84e8a7c73ca7
ckse93 <ckse93@gmail.com>
2019-10-20 19:00:05 -0500

    progressbar implemented

--- Flash-Chat-iOS12-master/Flash Chat/LogInViewController.swift
+++ Flash-Chat-iOS12-master/Flash Chat/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Firebase
-
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -26,11 +26,13 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
+        SVProgressHUD.show()
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print("error")
             } else {
                 print ("no error")
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
