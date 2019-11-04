0c60a2433d6bed602ec865dfeb331cde98e5db33
ckse93 <ckse93@gmail.com>
2019-11-04 08:53:35 -0600

    Chan business card app skeleton project uploaded

---
+++ ChanCard/ChanCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChanCard
+//
+//  Created by Chan Jung on 11/4/19.
+//  Copyright © 2019 Chan Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
