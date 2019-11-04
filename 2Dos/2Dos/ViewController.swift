6f8f1911824010b4c9d57d8becdc299c812f15e9
ckse93 <ckse93@gmail.com>
2019-11-03 21:26:05 -0600

    setting up skeleton project

---
+++ 2Dos/2Dos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  2Dos
+//
+//  Created by Chan Jung on 11/3/19.
+//  Copyright © 2019 Chan Jung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
