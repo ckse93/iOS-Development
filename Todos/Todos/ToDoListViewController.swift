3a8f8204ffd995a07023e34a88290f312d643e41
ckse93 <ckse93@gmail.com>
2019-11-05 09:20:51 -0600

    uploading skeleton project

---
+++ Todos/Todos/ToDoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todos
+//
+//  Created by Chan Jung on 11/5/19.
+//  Copyright © 2019 Chan Jung. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewListController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
