36f4faa64958d4325b23f721f7ef2311ce2254a5
ckse93 <ckse93@gmail.com>
2019-11-05 10:53:22 -0600

    works with hardcoded stuff being displayed

--- Todos/Todos/ToDoListViewController.swift
+++ Todos/Todos/ToDoListViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ToDoViewListController: UITableViewController {
+    
+    let itemArr = ["buy eggs", "cancel chiropractor", "make meme"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+// MARK ------- TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArr[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArr.count
+    }
+    
+    // MARK ---------------TableView Delegate
 }
 
